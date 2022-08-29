5df234be203f755fd7052f91c93c885902041492
Kevin Rue <kevinrue67@gmail.com>
2022-08-29 11:46:57 +0100

    first live stream version

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,99 @@
-from flask import Flask, render_template
+# import the necessary packages
+from flask import Response, Flask, render_template
+import threading
+import argparse 
+import datetime, time
+import imutils
+import cv2
 
+# initialize the output frame and a lock used to ensure thread-safe
+# exchanges of the output frames (useful when multiple browsers/tabs are viewing the stream)
+outputFrame = None
+lock = threading.Lock()
+ 
+# initialize a flask object
 app = Flask(__name__)
+ 
+cap = cv2.VideoCapture(0)
+cap_width = 1280
+cap_height = 720
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, cap_width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, cap_height)
+time.sleep(2.0)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    # return the rendered template
+    return render_template("index.html")
 
+def stream(frameCount):
+    global outputFrame, lock
+    if cap.isOpened():
+        # cv2.namedWindow(('IP camera DEMO'), cv2.WINDOW_AUTOSIZE)
+        while True:
+            ret_val, frame = cap.read()
+            time.sleep(0.5)
+            if frame.shape:
+                frame = cv2.resize(frame, (cap_width, cap_height))
+                frame = imutils.rotate(frame, 180)
+                with lock:
+                    outputFrame = frame.copy()
+            else:
+                continue
+    else:
+        print('camera open failed')
+
+def generate():
+    # grab global references to the output frame and lock variables
+    global outputFrame, lock
+ 
+    # loop over frames from the output stream
+    while True:
+        # wait until the lock is acquired
+        with lock:
+            # check if the output frame is available, otherwise skip
+            # the iteration of the loop
+            if outputFrame is None:
+                continue
+ 
+            # encode the frame in JPEG format
+            (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+ 
+            # ensure the frame was successfully encoded
+            if not flag:
+                continue
+ 
+        # yield the output frame in the byte format
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+            bytearray(encodedImage) + b'\r\n')
+
+@app.route("/video_feed")
+def video_feed():
+    # return the response generated along with the specific media
+    # type (mime type)
+    return Response(generate(),
+        mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+# check to see if this is the main thread of execution
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    # construct the argument parser and parse command line arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--ip", type=str, required=False, default='192.168.2.226',
+        help="ip address of the device")
+    ap.add_argument("-o", "--port", type=int, required=False, default=8000, 
+        help="ephemeral port number of the server (1024 to 65535)")
+    ap.add_argument("-f", "--frame-count", type=int, default=32,
+        help="# of frames used to construct the background model")
+    args = vars(ap.parse_args())
+
+    t = threading.Thread(target=stream, args=(args["frame_count"],))
+    t.daemon = True
+    t.start()
+ 
+    # start the flask app
+    app.run(host=args["ip"], port=args["port"], debug=True,
+        threaded=True, use_reloader=False)
+ 
+# release the video stream pointer
+cap.release()
+cv2.destroyAllWindows()
