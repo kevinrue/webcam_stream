e375c3dbd1bd5d6838e6b5ea165358c8d9dd1294
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-09-18 23:13:58 +0100

    move initialisation of argparse to module

--- promotion/src/promotion/cli/args.py
+++ promotion/src/promotion/cli/args.py
@@ -1,9 +1,26 @@
+import argparse
+
+
+def init_argparse():
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s [OPTION] [FILE]...",
+        description="Print or check SHA1 (160-bit) checksums.",
+    )
+    parser.add_argument(
+        "-v", "--version", action="version", version=f"{parser.prog} version 1.0.0"
+    )
+    parser.add_argument("-f", "--filename")
+    parser.add_argument("--fps")
+    return parser
+
+
 def process_filename(filename):
     if filename.isdigit():
         filename = int(filename)
     else:
         filename = filename
     return filename
+
 
 def process_fps(fps):
     fps = int(fps)
--- promotion/src/promotion/main.py
+++ promotion/src/promotion/main.py
@@ -1,30 +1,16 @@
-import argparse
 import sys
 import cli.args
 from video.stream import get_stream, loop_stream
 
 
 def main():
-    parser = init_argparse()
+    parser = cli.args.init_argparse()
     args = parser.parse_args()
     filename = cli.args.process_filename(args.filename)
     fps = cli.args.process_fps(args.fps)
     video_stream = get_stream(filename)
     loop_stream(video_stream, fps)
     video_stream.release()
-
-
-def init_argparse():
-    parser = argparse.ArgumentParser(
-        usage="%(prog)s [OPTION] [FILE]...",
-        description="Print or check SHA1 (160-bit) checksums.",
-    )
-    parser.add_argument(
-        "-v", "--version", action="version", version=f"{parser.prog} version 1.0.0"
-    )
-    parser.add_argument("-f", "--filename")
-    parser.add_argument("--fps")
-    return parser
 
 
 if __name__ == "__main__":
