5419a283acec807879fe802389c87cc75d021adb
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-09-18 13:26:24 +0100

    lint

--- promotion/src/promotion/video/stream.py
+++ promotion/src/promotion/video/stream.py
@@ -1,10 +1,11 @@
 import cv2
 
+
 def get_stream(filename):
     video_stream = cv2.VideoCapture(filename)
     if not video_stream.isOpened():
         raise IOError("Cannot open video stream")
-    return(video_stream)
+    return video_stream
 
 
 def loop_stream(video_stream):
