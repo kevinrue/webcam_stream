a45deb00012b770a00751e05316efdc90c39e3ac
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-09-19 21:07:02 +0100

    refactor new class App

--- promotion/src/promotion/main.py
+++ promotion/src/promotion/main.py
@@ -1,6 +1,6 @@
 import sys
 import cli.args
-from video.stream import get_stream, loop_stream
+from video.stream import App
 
 
 def main():
@@ -11,17 +11,18 @@ def main():
     resolution = cli.args.process_shape(args.resolution)
     baseline_frequency = cli.args.process_integer(args.baseline_frequency)
     baseline_frames = cli.args.process_integer(args.baseline_frames)
-    ksize = cli.args.process_shape(args.blur_ksize)
+    blur_ksize = cli.args.process_shape(args.blur_ksize)
     threshold_min = cli.args.process_integer(args.threshold_min)
     threshold_max = cli.args.process_integer(args.threshold_max)
     object_shape_min = cli.args.process_shape(args.object_shape_min)
     object_rectangle_color = cli.args.process_color(args.object_rectangle_color)
     object_rectangle_thickness = cli.args.process_integer(args.object_rectangle_thickness)
-    video_stream = get_stream(filename)
-    loop_stream(
-        video_stream, fps, resolution, baseline_frames, baseline_frequency, ksize, threshold_min, threshold_max,
-        object_shape_min, object_rectangle_color, object_rectangle_thickness)
-    video_stream.release()
+    app = App(
+        filename, fps, resolution, baseline_frames, baseline_frequency, blur_ksize, threshold_min,
+        threshold_max, object_shape_min, object_rectangle_color, object_rectangle_thickness)
+    app.open_stream()
+    app.loop()
+    
 
 
 if __name__ == "__main__":
--- promotion/src/promotion/video/stream.py
+++ promotion/src/promotion/video/stream.py
@@ -3,33 +3,46 @@ from video.baseline import Baseline
 from video.process import process_frame
 
 
-def get_stream(filename):
-    video_stream = cv2.VideoCapture(filename)
-    if not video_stream.isOpened():
-        raise IOError("Cannot open video stream")
-    return video_stream
+class App:
+    def __init__(
+        self, filename, fps, resolution, baseline_frames, baseline_frequency, blur_ksize, threshold_min,
+        threshold_max, object_shape_min, object_rectangle_color, object_rectangle_thickness
+    ):
+        self.filename = filename
+        self.fps = fps
+        self.blur_ksize = blur_ksize
+        self.threshold_min = threshold_min
+        self.threshold_max = threshold_max
+        self.object_shape_min = object_shape_min
+        self.object_rectangle_color = object_rectangle_color
+        self.object_rectangle_thickness = object_rectangle_thickness
+        self.baseline = Baseline(resolution=resolution, max_frames=baseline_frames, frequency=baseline_frequency)
+        self.stream = None
 
+    def open_stream(self):
+        self.stream = cv2.VideoCapture(self.filename)
+        if not self.stream.isOpened():
+            raise IOError("Cannot open video stream")
+    
+    def release_stream(self):
+        self.stream.release()
 
-def loop_stream(
-    video_stream, fps, resolution, baseline_frames, baseline_frequency, ksize, threshold_min, threshold_max,
-    object_shape_min, object_rectangle_color, object_rectangle_thickness
-):
-    baseline = Baseline(resolution=resolution, max_frames=baseline_frames, frequency=baseline_frequency)
-    ret, frame = video_stream.read()
-    while ret and frame.shape[0] > 0 and frame.shape[1] > 0:
+    def loop(self):
+        ret, frame = self.stream.read()
+        while ret and frame.shape[0] > 0 and frame.shape[1] > 0:
 
-        baseline.append_frame(frame)
-        baseline.compute_median_gray()
-        frame = process_frame(
-            frame, baseline, ksize, threshold_min, threshold_max, object_shape_min,
-            object_rectangle_color, object_rectangle_thickness)
+            self.baseline.append_frame(frame)
+            self.baseline.compute_median_gray()
+            frame = process_frame(
+                frame, self.baseline, self.blur_ksize, self.threshold_min, self.threshold_max, self.object_shape_min,
+                self.object_rectangle_color, self.object_rectangle_thickness)
 
-        # Display
-        cv2.imshow("Output", frame)
-        # Duration of each frame on screen
-        c = cv2.waitKey(int(1000 / fps))
-        if c == 27:
-            break
+            # Display
+            cv2.imshow("Output", frame)
+            # Duration of each frame on screen
+            c = cv2.waitKey(int(1000 / self.fps))
+            if c == 27:
+                break
 
-        # Read next frame
-        ret, frame = video_stream.read()
+            # Read next frame
+            ret, frame = self.stream.read()
