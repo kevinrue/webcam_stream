23f92402a4d720b4993d566cf90deca620efb86a
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2022-09-20 16:51:23 +0100

    move function as method

--- promotion/src/promotion/video/stream.py
+++ promotion/src/promotion/video/stream.py
@@ -1,6 +1,5 @@
 import cv2
 from video.baseline import Baseline
-from video.process import process_frame
 
 
 class App:
@@ -34,9 +33,7 @@ class App:
 
             self.baseline.append_frame(frame)
             self.baseline.compute_median_gray()
-            frame = process_frame(
-                frame, self.baseline, self.blur_ksize, self.threshold_min, self.threshold_max, self.detection_exclude_margins,
-                self.object_shape_min, self.object_rectangle_color, self.object_rectangle_thickness)
+            frame = self.process_frame(frame)
 
             # Display
             cv2.imshow("Output", frame)
@@ -47,3 +44,37 @@ class App:
 
             # Read next frame
             ret, frame = self.stream.read()
+    
+    def process_frame(self, frame):
+        if self.baseline.median_gray_frame is None:
+            return frame
+        if self.baseline.resolution is not None:
+            frame = cv2.resize(frame, (self.baseline.resolution[0], self.baseline.resolution[1]))
+        # Convert current frame to grayscale
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Calculate absolute difference of current frame and the median frame
+        difference_frame = cv2.absdiff(gray_frame, self.baseline.median_gray_frame)
+        # Apply Gaussian blur to smooth the frame of absolute differences
+        blurred = cv2.GaussianBlur(difference_frame, self.blur_ksize, 0)
+        # Apply threshold(s) to the smoothed difference
+        ret, tframe = cv2.threshold(
+            blurred, self.threshold_min, self.threshold_max, cv2.THRESH_TOZERO
+        )
+        # Find contours in the thresholded difference
+        (cnts, _) = cv2.findContours(
+            tframe.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
+        # Draw contours on the current frame
+        for cnt in cnts:
+            x, y, w, h = cv2.boundingRect(cnt)
+            if y > self.detection_exclude_margins[0]:
+                if h > self.object_shape_min[0] and w > self.object_shape_min[1]:
+                    cv2.rectangle(
+                        frame,
+                        (x, y),
+                        (x + w, y + h),
+                        self.object_rectangle_color,
+                        self.object_rectangle_thickness,
+                    )
+
+        return (frame)
